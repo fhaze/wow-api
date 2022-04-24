f8a24722daced2fc4c955d8e8b16bce00f6482f7
fhaze <eder_arm@icloud.com>
2022-04-24 10:15:37 -0300

    adding cors and logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,16 @@ import (
 	"wow-api/internal"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, "X-22-KEY"},
+	}))
+	e.Use(middleware.Logger())
 	internal.Assign(e)
 	e.Logger.Fatal(e.Start(":6000"))
 }
